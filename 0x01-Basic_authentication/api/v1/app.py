7da03bffe9b86bd3d8f3cd7ba8fea20dad9c6f6b
SlimjimmyMax <slimjimmymax@outlook.com>
2024-06-05 01:28:55 +0300

    Refactor BasicAuth class for improved readability and consistency

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,51 +2,58 @@
 """
 Route module for the API
 """
-from os import getenv
-from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
 import os
+
+from flask import Flask, jsonify, abort, request
+from flask_cors import CORS
+
+from api.v1.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.views import app_views
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 auth = None
-AUTH_TYPE = getenv("AUTH_TYPE")
+AUTH_TYPE = os.getenv("AUTH_TYPE")
 
 if AUTH_TYPE == "auth":
-    from api.v1.auth.auth import Auth
     auth = Auth()
 elif AUTH_TYPE == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
 
 @app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
+def not_found(error):
+    """
+    Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
-def unauthorized_error(error) -> str:
-    """ Unauthorized handler
+def unauthorized_error(error):
+    """
+    Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden_error(error) -> str:
-    """ Forbidden handler
+def forbidden_error(error):
+    """
+    Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
-def before_request() -> str:
-    """ Before Request Handler
+def before_request():
+    """
+    Before Request Handler
     Requests Validation
     """
     if auth is None:
@@ -67,6 +74,7 @@ def before_request() -> str:
 
 
 if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
+    host = os.getenv("API_HOST", "0.0.0.0")
+    port = os.getenv("API_PORT", "5000")
     app.run(host=host, port=port)
+
