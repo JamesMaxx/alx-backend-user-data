7d775ae19773c5dcf221681375b0e619a17a02dd
SlimjimmyMax <slimjimmymax@outlook.com>
2024-06-05 01:58:02 +0300

    chore: Refactor code for consistency and readability

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 """
-Route module for the API.
+Route module for the API
 """
-
 from os import getenv
+from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import CORS
+from flask_cors import (CORS, cross_origin)
+import os
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
+auth = None
 AUTH_TYPE = getenv("AUTH_TYPE")
 
 if AUTH_TYPE == "auth":
@@ -22,34 +24,30 @@ elif AUTH_TYPE == "basic_auth":
 
 
 @app.errorhandler(404)
-def not_found_error(error) -> str:
-    """
-    Not found handler.
+def not_found(error) -> str:
+    """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized_error(error) -> str:
-    """
-    Unauthorized handler.
+    """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden_error(error) -> str:
-    """
-    Forbidden handler.
+    """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
 def before_request() -> str:
-    """
-    Before Request Handler.
-    Requests Validation.
+    """ Before Request Handler
+    Requests Validation
     """
     if auth is None:
         return
@@ -67,8 +65,6 @@ def before_request() -> str:
     if auth.current_user(request) is None:
         abort(403)
 
-
-from api.v1.views import app_views
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
