97516fc9f369e1fb70df5231dc793bbbb5e59e28
SlimjimmyMax <slimjimmymax@outlook.com>
2024-06-05 01:54:47 +0300

    chore: Refactor code for consistency and readability

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,53 +2,49 @@
 """
 Route module for the API
 """
+
 from os import getenv
-from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
+from flask_cors import CORS
+
+from api.v1.views import app_views
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = None
+
 AUTH_TYPE = getenv("AUTH_TYPE")
 
 if AUTH_TYPE == "auth":
-    from api.v1.auth.auth import Auth
     auth = Auth()
 elif AUTH_TYPE == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
 
 @app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
-    """
+def not_found(error):
+    """Not found handler."""
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
-def unauthorized_error(error) -> str:
-    """ Unauthorized handler
-    """
+def unauthorized_error(error):
+    """Unauthorized handler."""
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden_error(error) -> str:
-    """ Forbidden handler
-    """
+def forbidden_error(error):
+    """Forbidden handler."""
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
-def before_request() -> str:
-    """ Before Request Handler
-    Requests Validation
-    """
+def before_request():
+    """Before request handler. Requests validation."""
     if auth is None:
         return
 
@@ -70,3 +66,4 @@ if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
+
