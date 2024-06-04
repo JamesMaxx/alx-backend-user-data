e5353112839f185611c2d18dd9b5e3f8cd90d420
SlimjimmyMax <slimjimmymax@outlook.com>
2024-06-05 01:22:12 +0300

    Refactor code for consistency and readability

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,96 +2,58 @@
 """
 Route module for the API
 """
-
 from os import getenv
-from flask import Flask, jsonify, abort, request
-from flask_cors import CORS, cross_origin
-
 from api.v1.views import app_views
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
-from models.user import User
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+AUTH_TYPE = getenv("AUTH_TYPE")
 
-
-def get_auth_type() -> str:
-    """
-    Get the type of authentication from the environment variable.
-
-    Returns:
-      - The type of authentication.
-    """
-    return getenv("AUTH_TYPE")
-
-
-def create_auth() -> Auth:
-    """
-    Create an instance of the authentication class based on the authentication type.
-
-    Returns:
-      - An instance of the authentication class.
-    """
-    auth_type = get_auth_type()
-    if auth_type == "auth":
-        return Auth()
-    elif auth_type == "basic_auth":
-        return BasicAuth()
-    else:
-        return None
-
-
-auth = create_auth()
+if AUTH_TYPE == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+elif AUTH_TYPE == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """
-    Not found handler
-
-    Returns:
-      - A JSON response with an error message.
+    """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized_error(error) -> str:
-    """
-    Unauthorized handler
-
-    Returns:
-      - A JSON response with an error message.
+    """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden_error(error) -> str:
-    """
-    Forbidden handler
-
-    Returns:
-      - A JSON response with an error message.
+    """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
 def before_request() -> str:
-    """
-    Before Request Handler
+    """ Before Request Handler
     Requests Validation
-
-    Returns:
-      - None if the authentication is not required for the request.
     """
     if auth is None:
         return
 
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
                       '/api/v1/forbidden/']
 
     if not auth.require_auth(request.path, excluded_paths):
